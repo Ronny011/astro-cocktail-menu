8eff1bb3dc21ae10ae36e15e050d4967cd0b3bce
Ronny011 <sylum.mail@gmail.com>
2026-02-07 15:04:01 +0200

    feat: Add Prettier configuration and integrate MDX, React, Tailwind CSS, and i18n support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ronny011.github.io",
   base: "/astro-cocktail-menu",
+  integrations: [mdx(), react()],
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ru", "he"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
